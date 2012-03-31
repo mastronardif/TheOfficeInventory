01b9895018451e0d08d3215ce007c09beea5bf68
mastronardif <mastronardif@gmail.com>
2012-03-31 19:42:12 -0400

    Initial commit

---
+++ Mvc3Razor/Controllers/MyXmlController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc3Razor.Controllers
+{
+    public class MyXmlController : Controller
+    {
+        //
+        // GET: /MyXml/
+
+        public ActionResult Office()
+        {
+            ViewBag.fuck = "<h1>The Office </h1>";
+            ViewBag.fn = "office2.htm";
+            return View();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult ViewPage1()
+        {
+            return View();
+        }
+
+
+
+    }
+
+}
---
+++ Mvc3Razor/MyHelpers/MyHelpersClass1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Mvc3Razor.MyHelpers
+{
+    public static class MyHelpersClass1
+    {
+
+        public static IHtmlString Fuck(string fn)
+        {
+            //string filePath = HttpContext.Current.Server.MapPath("~/App_Data/000.htm");
+            string filePath = HttpContext.Current.Server.MapPath("~/App_Data/"+fn);
+
+            //return new HtmlString(@"<b>C:\FxM\MSDev\another mvc\Mvc3Razor\MyHelpers\MyHelpersClass1.cs</b><br/>");
+            var streamReader = File.OpenText(filePath);
+            var markup = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return new HtmlString(markup);
+
+            //return new HtmlString(path);
+        }
+
+
+        public static IHtmlString ServerSideInclude(this HtmlHelper helper, string serverPath)
+        {
+            var filePath = HttpContext.Current.Server.MapPath(serverPath);
+
+            // load from file
+            var streamReader = File.OpenText(filePath);
+            var markup = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return new HtmlString(markup);
+        }
+
+
+    }
+}
