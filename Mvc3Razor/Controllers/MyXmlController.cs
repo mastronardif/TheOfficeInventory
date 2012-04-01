ac717f6cd25d821eeee8c20df1c74703e5aa2e96
mastronardif <mastronardif@gmail.com>
2012-04-01 18:40:15 -0400

    added more changes xsl stuff, moe html helpers

--- Mvc3Razor/Controllers/MyXmlController.cs
+++ Mvc3Razor/Controllers/MyXmlController.cs
@@ -11,6 +11,22 @@ namespace Mvc3Razor.Controllers
         //
         // GET: /MyXml/
 
+        public ActionResult Test22()
+        {
+            ViewBag.fuck = "<h1>hey ya big dummy!</h1>";
+            ViewBag.fnXml = "detail.xml";
+            ViewBag.fnXsl = "myTable.xsl";
+            return View("dummy");
+        }
+
+
+        public ActionResult Test()
+        {
+            ViewBag.fuck = "<h1>Test shit here!</h1>";
+            ViewBag.fn = "*.xml";
+            return View();
+        }
+
         public ActionResult Office()
         {
             ViewBag.fuck = "<h1>The Office </h1>";
--- Mvc3Razor/MyHelpers/MyHelpersClass1.cs
+++ Mvc3Razor/MyHelpers/MyHelpersClass1.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.Xml;
 
 namespace Mvc3Razor.MyHelpers
 {
@@ -36,6 +39,99 @@ namespace Mvc3Razor.MyHelpers
             streamReader.Close();
 
             return new HtmlString(markup);
+        }
+
+        public static IHtmlString ListFiles(string ext)
+        {
+            string filePath = HttpContext.Current.Server.MapPath("~/App_Data/");
+
+            string sep = ", <br/>";
+
+            var filteredFiles = Directory.GetFiles(filePath, "*.*")
+                                         .Where(file => file.EndsWith("xml") || file.EndsWith("xsl") || file.EndsWith("xslt"))
+                                         .ToList();
+
+            for (int iii=0; iii < filteredFiles.Count; iii++)
+            {
+                filteredFiles[iii] = Path.GetFileName(filteredFiles[iii]);
+            }
+
+            string str = String.Join(sep, filteredFiles);
+            
+            return  new HtmlString(str);
+        }
+
+        public static IHtmlString Transform00(string fnxml, string fnxsl)
+        {
+            string sXmlPath = HttpContext.Current.Server.MapPath("~/App_Data/" + fnxml); ;
+            string sXslPath = HttpContext.Current.Server.MapPath("~/App_Data/" + fnxsl); ;
+            return Transform(sXmlPath, sXslPath);
+        }
+        public static IHtmlString Transform(string sXmlPath, string sXslPath)
+        {
+            try
+            {
+                //load the Xml doc
+                XPathDocument myXPathDoc = new XPathDocument(sXmlPath);
+
+                XsltArgumentList args = new XsltArgumentList();
+
+                IDictionary<string, string> parameters = null;
+                if (parameters != null)
+                {
+                    foreach (string key in parameters.Keys)
+                    {
+                        args.AddParam(key, "", parameters[key]);
+                    }
+                }
+                XslCompiledTransform tt = new XslCompiledTransform();
+
+                //load the Xsl 
+                tt.Load(sXslPath);
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Parse;
+                settings.ValidationType = ValidationType.DTD;
+
+                //create the output stream
+                //XmlTextWriter myWriter = new XmlTextWriter("result.html", null);
+                //StringWriter writer = new StringWriter();
+
+                //do the actual transform of Xml
+                //myXslTrans.Transform(myXPathDoc, null, myWriter);
+
+                //System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                //StringWriter sw = new StringWriter(sb);
+
+                using (XmlReader reader = XmlReader.Create(sXmlPath, settings))
+                {
+                    StringWriter writer = new StringWriter();
+                    tt.Transform(reader, args, writer);
+                    return new HtmlString(writer.ToString());
+                }
+
+
+
+
+                //myXslTrans.Transform(myXPathDoc, null, sw);
+                //sw.Close();
+                //string results = sb.ToString();
+                                
+                //Console.WriteLine(results);
+
+
+                //myWriter.Close();
+                //return new HtmlString(results);
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception: {0}", e.ToString());
+                return new HtmlString(e.ToString());
+            }
+
         }
 
 
