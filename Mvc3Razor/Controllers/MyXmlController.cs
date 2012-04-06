76c8d3758707e63ea4313d89c8d133606a057210
mastronardif <mastronardif@gmail.com>
2012-04-05 23:11:16 -0400

    justonedb pray for me  Please enter the commit message for your changes. Lines starting

--- Mvc3Razor/Controllers/MyXmlController.cs
+++ Mvc3Razor/Controllers/MyXmlController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.IO;
+using RestSharp;
 
 namespace Mvc3Razor.Controllers
 {
@@ -10,6 +13,79 @@ namespace Mvc3Razor.Controllers
     {
         //
         // GET: /MyXml/
+        public ActionResult JustOneDb()
+        {
+            ViewBag.fuck = "<h1>Test JustONeDb!</h1>";
+            ///////////
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            try
+            {
+                {
+
+                    var client = new RestClient();
+                    client.BaseUrl = "https://77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/";
+                    //client.BaseUrl = "https://api-dev.bugzilla.mozilla.org/1.1/";
+                    client.Authenticator = new HttpBasicAuthenticator("zn0lvkpdhdxb70l2ub4", "iy59bj7rh0z6uurshn5e7i41lb3fiwuh");
+
+                    var request22 = new RestRequest();
+                    request22.Resource = "/session";
+
+                    RestResponse response22 = client.Execute(request22);
+                    return Content(response22.Content);
+                }
+
+
+                String Xml;
+                // Create the web request 
+                // curl -k -X POST https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session  
+                //curl -k -X GET             https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session
+                //string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
+
+                // Fm 4/5/12
+                //curl -k -X GET https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session
+                //string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
+                string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
+
+                request = (HttpWebRequest)WebRequest.Create(url);
+
+                request.Method = "GET";
+
+                //request.Method = "HEAD";
+                //request.AllowAutoRedirect = false;
+                request.Credentials = CredentialCache.DefaultCredentials;
+
+                // Ignore Certificate validation failures (aka untrusted certificate + certificate chains)
+                ServicePointManager.ServerCertificateValidationCallback = ((sender, certificate, chain, sslPolicyErrors) => true);
+
+                ((HttpWebRequest)request).AllowWriteStreamBuffering = true;
+
+                // Get response 
+                using (response = (HttpWebResponse)request.GetResponse())
+                {
+                    // Get the response stream 
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    Xml = reader.ReadToEnd();
+                }
+                return Content(Xml);
+            }
+            catch (Exception ee)
+            {
+                return Content(ee.ToString());
+            
+            }
+            //////////////
+
+
+
+
+
+
+
+            ViewBag.fn = "*.xml";
+            return View();
+        }
+
 
         public ActionResult Test22()
         {
