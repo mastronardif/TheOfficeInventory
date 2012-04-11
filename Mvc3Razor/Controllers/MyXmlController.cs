cd77e200cf52746500890cfc4551bc30fe0b42b0
mastronardif <mastronardif@gmail.com>
2012-04-11 09:44:26 -0400

    json for pinterest

--- Mvc3Razor/Controllers/MyXmlController.cs
+++ Mvc3Razor/Controllers/MyXmlController.cs
@@ -7,10 +7,108 @@ using System.Net;
 using System.IO;
 using RestSharp;
 
+using System.Xml;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Configuration;
+using Newtonsoft.Json;
+
 namespace Mvc3Razor.Controllers
 {
     public class MyXmlController : Controller
     {
+
+        // callback used to validate the certificate in an SSL conversation
+        private static bool ValidateRemoteCertificate(
+        object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors policyErrors
+        )
+        {
+            if (Convert.ToBoolean(ConfigurationManager.AppSettings["IgnoreSslErrors"]))
+            {
+                // allow any old dodgy certificate...
+                return true;
+            }
+            else
+            {
+                //return policyErrors == SslPolicyErrors.None;
+                // FM
+                return true;
+            }
+        }
+
+        private static string MakeRequest(string uri, string method, WebProxy proxy)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
+            webRequest.AllowAutoRedirect = true;
+            webRequest.Method = method;
+
+            // allows for validation of SSL conversations
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(
+                ValidateRemoteCertificate
+            );
+
+            if (proxy != null)
+            {
+                webRequest.Proxy = proxy;
+            }
+
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)webRequest.GetResponse();
+
+                using (Stream s = response.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(s))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+                if (response != null)
+                    response.Close();
+            }
+        }
+
+        public ActionResult Pinterest()
+        {
+            try
+            {
+                var client = new RestClient();
+                client.BaseUrl = "https://api.pinterest.com/v2/popular/";
+
+                var request22 = new RestRequest();
+
+                RestResponse response22 = client.Execute(request22);
+
+                string myJsonString = response22.Content;
+                System.Text.StringBuilder myJsonString22 = new System.Text.StringBuilder();
+                myJsonString22.Append("{ \"?xml\": { \"@version\": \"1.0\", \"@standalone\": \"no\" }, \"root\":" + myJsonString + "}");
+
+                string json = myJsonString22.ToString();
+                System.Xml.XmlDocument xmlDocument = Newtonsoft.Json.JsonConvert.DeserializeXmlNode(json);
+                System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter("json.xml", null);
+                xmlTextWriter.Formatting = System.Xml.Formatting.Indented;
+                xmlDocument.Save(xmlTextWriter);
+
+                string resutls = (xmlDocument).InnerXml;
+
+                Response.ContentType = "text/xml";
+                return Content(resutls);
+                //return Content(response22.Content);
+            }
+            catch (Exception ee)
+            {
+                return Content(ee.ToString());
+            }
+            
+        }
+
         //
         // GET: /MyXml/
         public ActionResult JustOneDb()
@@ -21,6 +119,7 @@ namespace Mvc3Razor.Controllers
             HttpWebResponse response = null;
             try
             {
+                if (1 == 21)
                 {
 
                     var client = new RestClient();
@@ -29,10 +128,11 @@ namespace Mvc3Razor.Controllers
                     client.Authenticator = new HttpBasicAuthenticator("zn0lvkpdhdxb70l2ub4", "iy59bj7rh0z6uurshn5e7i41lb3fiwuh");
 
                     var request22 = new RestRequest();
-                    request22.Resource = "/session";
+                    //request22.Resource = "/session";
 
                     RestResponse response22 = client.Execute(request22);
-                    return Content(response22.Content);
+                    return Content("h f");
+                    //return Content(response22.Content);
                 }
 
 
@@ -40,34 +140,48 @@ namespace Mvc3Razor.Controllers
                 // Create the web request 
                 // curl -k -X POST https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session  
                 //curl -k -X GET             https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session
-                //string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
-
+                string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
+                //                string url = @"https://api.pinterest.com/v2/popular/";
                 // Fm 4/5/12
                 //curl -k -X GET https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session
                 //string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
-                string url = @"https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
+                //string url = @"https://77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session";
 
+                if(1==15)
+                { 
+                    string strResp =  MakeRequest("https://zn0lvkpdhdxb70l2ub4:iy59bj7rh0z6uurshn5e7i41lb3fiwuh@77.92.68.105:31415/justonedb/database/n10lvkpdhdxei0l2uja/session",
+                                                  "GET", null);
+
+                    return Content(strResp);
+                
+                }
+
+                if (1==1)
+                {
                 request = (HttpWebRequest)WebRequest.Create(url);
 
                 request.Method = "GET";
 
                 //request.Method = "HEAD";
-                //request.AllowAutoRedirect = false;
-                request.Credentials = CredentialCache.DefaultCredentials;
+                //request.AllowAutoRedirect = true;
+                //request.Credentials = CredentialCache.DefaultCredentials;
+                //request.Credentials = new NetworkCredential("zn0lvkpdhdxb70l2ub4", "zn0lvkpdhdxb70l2ub4");
+
 
                 // Ignore Certificate validation failures (aka untrusted certificate + certificate chains)
-                ServicePointManager.ServerCertificateValidationCallback = ((sender, certificate, chain, sslPolicyErrors) => true);
+                //ServicePointManager.ServerCertificateValidationCallback = ((sender, certificate, chain, sslPolicyErrors) => true);
 
                 ((HttpWebRequest)request).AllowWriteStreamBuffering = true;
 
                 // Get response 
-                using (response = (HttpWebResponse)request.GetResponse())
+                using (response = (HttpWebResponse)request.GetResponse() as HttpWebResponse)
                 {
                     // Get the response stream 
                     StreamReader reader = new StreamReader(response.GetResponseStream());
                     Xml = reader.ReadToEnd();
                 }
                 return Content(Xml);
+                }
             }
             catch (Exception ee)
             {
@@ -75,12 +189,6 @@ namespace Mvc3Razor.Controllers
             
             }
             //////////////
-
-
-
-
-
-
 
             ViewBag.fn = "*.xml";
             return View();
